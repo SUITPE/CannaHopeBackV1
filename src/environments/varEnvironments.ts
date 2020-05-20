685a6c78471b0f9ce3d5611cc79de7cc430806b0
Jonathan <jonathan962911@gmail.com>
2020-05-19 23:31:35 -0500

    cambiando puerto en db

--- src/environments/varEnvironments.ts
+++ src/environments/varEnvironments.ts
@@ -13,7 +13,7 @@ export const environments = {
     getDbUrl(): string {
 
         if (currentEnv === 'PROD') {
-            return 'mongodb://167.71.119.143:27017';
+            return 'mongodb://167.71.119.143:27017/cannahope';
         } else {
             return 'mongodb+srv://jhony:7W8PfRWM1Hqn3sAw@cluster0-8i0r3.mongodb.net/test?retryWrites=true&w=majority';
         }
